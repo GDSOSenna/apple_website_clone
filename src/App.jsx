e2fbfe55b3c64bb9f31c0abdafdf1d903b6e1ce3
GDSOSenna <gustavosenna09@hotmail.com>
2025-04-29 10:43:28 -0300

    ModelView implementation :sparkles:

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Highlights from "./components/Highlights"
+import Model from "./components/Model"
 
 const App = () => {
 
@@ -12,6 +13,7 @@ const App = () => {
       <Navbar></Navbar>
       <Hero></Hero>
       <Highlights></Highlights>
+      <Model></Model>
     </main>
     </>
   )
